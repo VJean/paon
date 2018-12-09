c38754c24c134db872c8818c0be59549363251d7
VJean <jean@vintache.eu>
2018-12-09 19:20:40 +0100

    add SQLAlchemy conf settings to disable warning on startup

--- default_config.py
+++ default_config.py
@@ -2,6 +2,7 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 DEBUG = False
 SECRET_KEY = 'your_secret_key'
 WTF_CSRF_ENABLED = True
